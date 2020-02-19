9a05a576c666d0b7055637124fdd53b3ad3d3e50
Tim <Tim.Kreger@iflix.com>
2020-02-19 14:29:43 +1100

    [master] Updated direction files

--- Source/Direction.swift
+++ Source/Direction.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2020 audioreactive. All rights reserved.
 //
 
-import Foundation
+enum Direction: String {
+    case north = "NORTH"
+    case east = "EAST"
+    case south = "SOUTH"
+    case west = "WEST"
+
+    func vector(from: Direction) -> (Int, Int) {
+        switch from {
+        case .north:
+            return (0, 1)
+        case .east:
+            return (1, 0)
+        case .south:
+            return (0, -1)
+        case .west:
+            return (-1, 0)
+    
+        }
+    }
+
+    func rotateLeft(current: Direction) -> Direction {
+        switch current {
+        case .north:
+            return .west
+        case .west:
+            return .south
+        case .south:
+            return .east
+        case .east:
+            return .north
+        }
+    }
+
+    func rotateRight(current: Direction) -> Direction {
+           switch current {
+           case .north:
+               return .east
+           case .east:
+               return .south
+           case .south:
+               return .west
+           case .west:
+               return .north
+           }
+    }
+
+    func move(forward: Direction, from: (Int, Int)) -> (Int, Int) {
+        let transform = forward.vector(from: forward)
+        return (transform.0 + from.0, transform.1 + from.1)
+    }
+}
+
--- ToyRobotTests/DirectionTests.swift
+++ ToyRobotTests/DirectionTests.swift
@@ -7,27 +7,71 @@
 //
 
 import XCTest
+import ToyRobot
 
 class DirectionTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_initializer_with_valid_value() {
+        let direction = Direction(rawValue: "NORTH")
+        XCTAssert(direction == Direction.north)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_initializer_with_invalid_value() {
+        let direction = Direction(rawValue: "BLAT")
+        XCTAssertNil(direction)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_vector_for_north() {
+        let direction = Direction(rawValue: "NORTH")!
+        XCTAssertTrue(direction.vector(from: direction) == (0, 1))
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_vector_for_east() {
+        let direction = Direction(rawValue: "EAST")!
+        XCTAssertTrue(direction.vector(from: direction) == (1, 0))
+    }
+    
+    func test_vector_for_south() {
+        let direction = Direction(rawValue: "SOUTH")!
+        XCTAssertTrue(direction.vector(from: direction) == (0, -1))
+    }
+    
+    func test_vector_for_west() {
+        let direction = Direction(rawValue: "WEST")!
+        XCTAssertTrue(direction.vector(from: direction) == (-1, 0))
+    }
+    
+    // Will test all in one test as also need test wrap around
+    func test_rotation_left() {
+        var direction = Direction(rawValue: "NORTH")!
+        
+        direction = direction.rotateLeft(current: direction)
+        XCTAssertTrue(direction == .west)
+        
+        direction = direction.rotateLeft(current: direction)
+        XCTAssertTrue(direction == .south)
+        
+        direction = direction.rotateLeft(current: direction)
+        XCTAssertTrue(direction == .east)
+        
+        direction = direction.rotateLeft(current: direction)
+        XCTAssertTrue(direction == .north)
+    }
+    
+    // Will test all in one test as also need test wrap around
+    func test_rotation_right() {
+        var direction = Direction(rawValue: "NORTH")!
+        
+        direction = direction.rotateRight(current: direction)
+        XCTAssertTrue(direction == .east)
+        
+        direction = direction.rotateRight(current: direction)
+        XCTAssertTrue(direction == .south)
+        
+        direction = direction.rotateRight(current: direction)
+        XCTAssertTrue(direction == .west)
+        
+        direction = direction.rotateRight(current: direction)
+        XCTAssertTrue(direction == .north)
     }
 
 }
