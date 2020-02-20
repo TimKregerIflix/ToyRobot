72dd3c976467b907054bdcebb8219fd5245d397b
Tim <Tim.Kreger@iflix.com>
2020-02-20 16:16:19 +1100

    [executable] Added excutable refactored position parsing added swift package for CI

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ToyRobotPackage",
+    platforms: [
+        // Add support for all platforms starting from a specific version.
+        .macOS(.v10_15),
+    ],
+    products: [
+    ],
+    targets: [
+        .target(name: "ToyRobot", dependencies: [], path: "Source"),
+        .testTarget(name: "ToyRobotTests", dependencies: [], path: "ToyRobotTests")
+    ]
+)
